de58da4c721483579c8b6bcf0b6732fef43179cd
Inji <chldlswl1212@gmail.com>
2021-04-22 19:04:52 +0900

    5.6.3 Softmax-with-Loss layer

---
+++ Ch05/softmaxwithloss.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def softmax(a):
+	exp_a = np.exp(a)
+	sum_exp_a = np.sum(exp_a)
+	y = exp_a / sum_exp_a
+	return y
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 손실
+        self.y = None # Softmax의 출력
+        self.t = None # 정답 레이블(원-핫 벡터)
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
